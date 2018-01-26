5e16a8409e1d412cbee77544587dbf2dd39cf2c7
Chris Deaton <cldeaton@liberty.edu>
2018-01-26 01:48:55 -0500

    changes to carousel

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.fade').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    draggable: true
+  });
+});
