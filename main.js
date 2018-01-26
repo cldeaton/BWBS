f0704666da7a483425218cb2475631fb770fca66
Chris Deaton <cldeaton@liberty.edu>
2018-01-26 15:12:38 -0500

    added css and logo slide

--- main.js
+++ main.js
@@ -6,7 +6,39 @@ $(document).ready(function(){
     speed: 500,
     fade: true,
     cssEase: 'linear',
-    arrows: false,
+    arrows: true,
     draggable: true
+  });
+});
+
+
+$(document).ready(function(){
+  $(".center").slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoplay: true,
+    speed: 500,
+    draggable: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 });
