a01d64a51825ac56514c93c3c4a62831271a394a
Chris Deaton <cldeaton@liberty.edu>
2018-01-28 22:01:27 -0500

    routes added/refactored

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
-var bootstrap = require("boostrap");
+var express  = require("express"),
+    app      = express();
+
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 
 
-//Integrate Bootstrap
 
 
 
 
-
-
-//Index Route
-
-//Info Route
-
-//Product Route
+//Home Route
+app.get("/", function(req, res){
+  res.render("home");
+});
+app.get("/home", function(req, res){
+  res.render("home");
+});
 
 //Service Routes=============================
+app.get("/services", function(req, res){
+  res.render("services");
+});
 
 //Community Routes===========================
+app.get("/community", function(req, res){
+  res.render("community");
+});
+
+//Info Route
+app.get("/about_us", function(req, res){
+  res.render("about_us");
+});
+
+//Product Route
+app.get("/products", function(req, res){
+  res.render("products");
+});
 
 //Blog Route
+app.get("/blog", function(req, res){
+  res.render("blog");
+});
+
+
+
+//listening Route
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server has started!!");
+});
