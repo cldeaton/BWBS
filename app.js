00f4f50ab9f8e9815255623ebafd39680bba0e7b
Chris Deaton <cldeaton@liberty.edu>
2018-03-01 17:16:24 -0500

    refactor changed_temp

--- app.js
+++ app.js
@@ -5,94 +5,94 @@ var express  = require("express"),
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-var routePaths = [
-  'home',
-  'repairs',
-  'fitting',
-  'rental',
-  'coaching',
-  'community',
-  'local_races',
-  'local_rides',
-  'group_rides',
-  'bikes',
-  'clothing',
-  'accessories',
-  'racks',
-  'about_us'
-]
-
-routePaths.forEach((name) => {
-  app.get('/' + name, (req, res) => {
-    res.render(name);
-  });
-})
+// var routePaths = [
+//   'home',
+//   'repairs',
+//   'fitting',
+//   'rental',
+//   'coaching',
+//   'community',
+//   'local_races',
+//   'local_rides',
+//   'group_rides',
+//   'bikes',
+//   'clothing',
+//   'accessories',
+//   'racks',
+//   'about_us'
+//
+// ]
+// routePaths.forEach((name) => {
+//   app.get('/' + name, (req, res) => {
+//     res.render(name);
+//   });
+// })
 
 
 
 
 
 //Home Route
-// app.get("/", function(req, res){
-//   res.render("home");
-// });
-// app.get("/home", function(req, res){
-//   res.render("home");
-// });
+app.get("/", function(req, res){
+  res.render("home");
+});
+app.get("/home", function(req, res){
+  res.render("home");
+});
 
 //Service Routes=============================
-// app.get("/repairs", function(req, res){
-//   res.render("repairs");
-// });
-// app.get("/fitting", function(req, res){
-//   res.render("fitting");
-// });
-// app.get("/rentals", function(req, res){
-//   res.render("rentals");
-// });
-// app.get("/coaching", function(req, res){
-//   res.render("coaching");
-// });
+app.get("/repairs", function(req, res){
+  res.render("repairs");
+});
+app.get("/fitting", function(req, res){
+  res.render("fitting");
+});
+app.get("/rentals", function(req, res){
+  res.render("rentals");
+});
+app.get("/coaching", function(req, res){
+  res.render("coaching");
+});
 
 
 
 //Community Routes===========================
-// app.get("/community", function(req, res){
-//   res.render("community");
-// });
-// app.get("/local_races", function(req, res){
-//   res.render("local_races");
-// });
-//
-// app.get("/local_rides", function(req, res){
-//   res.render("local_rides");
-// });
-// app.get("/group_rides", function(req, res){
-//   res.render("group_rides");
-// });
+app.get("/community", function(req, res){
+  res.render("community");
+});
+app.get("/local_races", function(req, res){
+  res.render("local_races");
+});
+
+app.get("/local_rides", function(req, res){
+  res.render("local_rides");
+});
+app.get("/group_rides", function(req, res){
+  res.render("group_rides");
+});
 
 
 //Product Route
-// app.get("/products", function(req, res){
-//   res.render("products");
-// });
-// app.get("/bikes", function(req, res){
-//   res.render("bikes");
-// });
-// app.get("/clothing", function(req, res){
-//   res.render("clothing");
-// });
-// app.get("/accessories", function(req, res){
-//   res.render("accessories");
-// });
-// app.get("/racks", function(req, res){
-//   res.render("racks");
-// });
+app.get("/products", function(req, res){
+  res.render("products");
+});
+app.get("/bikes", function(req, res){
+  res.render("bikes");
+});
+app.get("/clothing", function(req, res){
+  res.render("clothing");
+});
+app.get("/accessories", function(req, res){
+  res.render("accessories");
+});
+app.get("/racks", function(req, res){
+  res.render("racks");
+});
 
 //Info Route
-// app.get("/about_us", function(req, res){
-//   res.render("about_us");
-// });
+app.get("/about_us", function(req, res){
+  res.render("about_us");
+});
 
 
 //Blog Route
